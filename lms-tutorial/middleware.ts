6fa72d4e776621bc5c988971feb8f0b811add745
Ubuntu <labuser@ubuntu2204.ruw23wbxviku1njuitdgnpqloc.rx.internal.cloudapp.net>
2024-07-24 11:07:56 +0000

    Updated Files

--- lms-tutorial/middleware.ts
+++ lms-tutorial/middleware.ts
@@ -1,6 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/text"]
+});
+
 
 export const config = {
   matcher: [
